1f3157ae92e68a32fd3647c7e8eb6a67f0df38d3
franckoline <franckoline@hotmail.com>
2026-04-21 12:30:36 +0100

    first commit

---
+++ phase1/session1/setup.py
@@ -0,0 +1,39 @@
+"""
+Session: One
+Date: 18th April 2026 10am-12noon
+focus: Setup (Python, Git, Google colab)
+Facilitator: Olatunbosun
+"""
+
+# Installation of Python
+"""
+Step 1: download python from python.org/downloads
+double click and follow required instructions to install complete
+Ensure python is added to path.
+"""
+
+# Install VS Code
+"""
+Download visual studio code from 
+https://code.visualstudio.com/download
+follow the instructions to complete the installation
+"""
+
+# Install Git
+"""
+Download git for Windows/Mac/Linux from
+https://git-scm.com/install/windows
+follow the instruction"""
+
+
+# Understanding Terminal
+"""
+cd - Change directory (foward or backward with ..)
+mv - Move folder or files (optional -r flag)
+rm - remove folder of files (optional -r flag)
+cp - copy file or folder (optional -a flag)
+pwd - view the path
+mkdir - make a new folder
+code . - launching new vs code on the from the current directory where you are on the terminal
+ls -  list files and folders in the current directory
+"""
---
+++ phase1/session2/virtual_syntax.py
@@ -0,0 +1,31 @@
+"""
+Session: One
+Date: 21st April 2026 10:30am-12:30pm
+focus: Setup (virtual environment, git)
+Facilitator: Olatunbosun
+"""
+
+# virtual Environment
+"""
+python -m venv 'choice name for the venv' - If you are creating it right in the 
+                                            directory where you are
+python -m venv /path/path/choice-name-for-the-venv - if your are pointing it to a directory
+
+Activation of Virtual Environment
+
+Step 1: .venvname/Scripts/Activate.ps1 - Go through if your system permit 
+        but sometime you have user policy error.
+        If the code run without error skip step 2
+Step 2: User Policy Error correction:
+Set-ExecutionPolicy -Scope process -ExecutionPolicy Bypass
+
+Step 3: Reinitiate step 1 again
+"""
+# Installing your first package
+"""
+verify installed packages - pip freeze
+pip install panda,
+pip install numpy
+
+verify installed packages again
+"""
