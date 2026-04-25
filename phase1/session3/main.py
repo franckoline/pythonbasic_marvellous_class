3fb9c9f0dd9fe27440567829dd8e9bd4e91f4125
marvellous <marvellouskevin06@gmail.com>
2026-04-25 13:18:15 +0100

    session3 code

---
+++ phase1/session3/main.py
@@ -0,0 +1,26 @@
+"""x = 37
+y = 45.0
+w = "2, 4, 7"
+print(x)
+print(y)
+print(w) 
+
+print(type(x))
+print(type(w))
+Name = "Marvellous"
+department = "software eng"
+Course = "computing"
+
+print(Name)
+print(department)
+print(Course)
+"""
+
+Name = str(input("what is your name: "))
+level = int(input("what level you're in: "))
+department = str(input("what's you're department"))
+
+
+print(Name)
+print(level)
+print(department)
