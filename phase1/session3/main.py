c08a081a0cd59a2dc40a6ae2adbc42287b214c6a
Elvis2001 <elvissamuel46@gmail.com>
2026-04-25 13:17:07 +0100

    Session 3

---
+++ phase1/session3/main.py
@@ -0,0 +1,35 @@
+"""
+x = 20
+y = 10
+z = 1.2
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+
+
+x="John"
+Name = "John"
+
+print(x, Name)
+
+"""
+
+
+
+Name = str(input("What is your Name: "))
+print(f"Hello {Name}, you're welcome")
+
+Level = int(input("insert your level in the Uni: "))
+print(f"It seems you are a {Level} level student")
+
+Course = str(input("What's your course of study? : "))
+print(f"Hmmmm, {Course} is a very nice course of study")
+
+
+
+
+#print(type(Name))
+
