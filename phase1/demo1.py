276233faea23d02861189b444e06a349a015dae1
marvellous <marvellouskevin06@gmail.com>
2026-04-30 14:01:49 +0100

    demo1.py

---
+++ phase1/demo1.py
@@ -0,0 +1,16 @@
+'''a = 56
+b = 7676
+
+if a < b: 
+    print("false")
+else:   print("true")'''
+
+Date = input("whats today's date? ")
+print(f"Is it really {Date} already?")
+
+plans = input("What are your plans for today? ")
+print(f"omo, {plans}?? for this hot weather?? oyanau")
+
+print("have fun!!!")
+
+
