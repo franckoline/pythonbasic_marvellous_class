053eadf97d9c21b442babda909b5fd80f78fb706
albadarawi <albadarawi@gmail.com>
2026-05-09 13:25:53 +0100

    Numeric dData and Arithmetic Operators

---
+++ phase1/session3/data.py
@@ -0,0 +1,27 @@
+print(2*3)
+number1 = 24
+number2 = 3.85
+print(number1*number2)
+
+# Numbers works with Arithmetic and assignment operator.
+print("-"*15)
+print("Numeric & Operator - Arithmetics\n")
+print("Division: ",number1/number2)
+print("Addition: ",number1+number2)
+print("Subtraction: ",number1-number2)
+print("Multiplication: ",number1*number2)
+print("Division: ",number1/number2)
+print("Floor Division: ",number1//number2)
+print("Addition: ",number1+number2)
+print("Modulus: ",number1%number2)
+
+print("\numeric  operator - Assignment\n")
+print("Equivalent: ",number1==number2)
+number1  +=20
+print("Increment: ",number1)
+number1 -=20
+print("Decrement: ",number1)
+number1 /=2
+print("Division: ",number1)
+
+print("\n-"*15)
