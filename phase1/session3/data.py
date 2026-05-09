0555e3bb843a656d855f63ed0014aac524cb2ce0
franckoline <franckoline@hotmail.com>
2026-05-09 13:04:08 +0100

    numeric with arithmetic and assignment operators

---
+++ phase1/session3/data.py
@@ -0,0 +1,38 @@
+"""
+Session: One
+Date: 9th May 2026 10:30am-12:30pm
+focus: Setup (data types, variable)
+Facilitator: Olatunbosun
+"""
+
+"""
+Data type Numbers (int, float, complex)
+Data type String (str)
+Data type Boolean (bool)
+Data type List (list) 
+"""
+
+print(2*2)
+number1 = 24
+number2 = 3.56
+print(number1*number2)
+
+# Numbers works with Arithmetic and assignment operator.
+print("-"*15)
+print("Numeric & Operator - Arithmetics\n")
+print("Division: ",number1/number2)
+print("Addition: ",number1+number2)
+print("Subtraction: ",number1-number2)
+print("Multiplication: ",number1*number2)
+print("Floor Division: ",number1//number2)
+print("Modulus: ",number1%number2)
+
+print("\nNumeric & Operator - Assignment\n")
+print("Equivalent: ",number1==number2)
+number1 +=20
+print("Increment: ",number1)
+number1 -=20
+print("Decrement: ",number1)
+number1 /=2
+print("Division: ",number1)
+
