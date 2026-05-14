011e401825471f0dd1d4e5f77371e16128221ce0
franckoline <franckoline@hotmail.com>
2026-05-14 10:28:37 +0100

    folder sturcture for phase2

--- phase1/session3/data.py
+++ phase1/session3/data.py
@@ -18,7 +18,7 @@ number2 = 3.56
 print(number1*number2)
 
 # Numbers works with Arithmetic and assignment operator.
-print("-"*15)
+print("\n-"*15)
 print("Numeric & Operator - Arithmetics\n")
 print("Division: ",number1/number2)
 print("Addition: ",number1+number2)
