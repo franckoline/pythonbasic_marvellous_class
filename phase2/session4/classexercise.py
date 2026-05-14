ed7b295e25a83ad4fa8f89265078b2e0ebb6dfca
marvellous <marvellouskevin06@gmail.com>
2026-05-14 13:07:24 +0100

    session5 revised

--- phase2/session4/classexercise.py
+++ phase2/session4/classexercise.py
@@ -1,5 +1,5 @@
-  #num1
-#business_name =str(input("What is the Business Name?: ")) 
+# num1
+#bjusiness_name =str(input("What is the Business Name?: ")) 
 #owner_name = str(input("What is the Owners Name?: "))
 #owner_age = int(input("What is the Owners Age?: "))
 #business_type = str(input("Type of Business?: "))
@@ -10,13 +10,13 @@
 #is_the_business_registered = str(input("Is the Business Registered (yes/no)?: "))
 #business_tax_id = str(input("Does the Business have a tax ID (yes/no)?: "))
  
-"""if owner_age >= 18:
+'''if owner_age >= 18:
       
-    print("You are Approved to go Ahead.")
+    print(f"hey {owner_name}, your are up to the required age! You are approved to go ahead! ")
 else:
-    print("Not Approved to Proceed.")"""    
-
-
+    print(f"hey {owner_name}, we're sorry to inform you that {owner_age} did not meet the required age.")
+'''
+#-----------------------------------------------------------------------------------
    #num2
 '''monthly_revenue =float(input("What is your Monthly Revenue?: "))
 time_in_business =int(input("How many years have you been in the Business?: "))
@@ -24,10 +24,15 @@ business_registered =str(input("Is the Business Registered?: "))
 business_tax_id =str(input("Does the Business have a Tax ID?: "))
 
 if monthly_revenue < 50000 and time_in_business >= 2:
+    print("you are eligible")
     if business_registered == 'yes' and business_tax_id == 'yes':
         print("You are Eligible!")
     else:
-        print("you are not Eligible")'''
+        print("you are not Eligible")
+else:
+    print("you are not eligible")'''   
+
+#--------------------------------------------------------------------------------
 
 
 
@@ -40,6 +45,8 @@ if revenue > 1000000 or employees < 2 or registered == 'no':
     print("Fraudulent activity detected")
 else:
     print("Good to go!")'''  
+
+#-----------------------------------------------------------------------
 
 
    #num4
@@ -54,23 +61,57 @@ elif annual_revenue <= 1000000:
 else:
     print("This is a Large Coporation")'''
 
+#-----------------------------------------------------------------
+
 
     #num5
-annul_revenue = int(input("What is your Annual Revenue?: "))
+'''annul_revenue = int(input("What is your Annual Revenue?: "))
 bussiness_years = int(input("How old is the Business(years)?: "))
 
 if annul_revenue > 100000 or bussiness_years > 10:
     print("You are Qualified")
 else:
-    print("you are not Qualified")        
+    print("dear user i am sorry to inform you that your information does not meet our specifications.")'''       
+
+#------------------------------------------------------------------------
+    
+
+#num6
+'''years_in_operations = int(input("How many years have you been in operation?: "))
+annual_revenue = int(input("What is your annual Revenue?: "))
+num_of_employees = int(input("How many employees do you have?: "))
+country  = str(input("What country is the bussiness in?: "))
+
+if years_in_operations > 5 and annual_revenue > 200000 and num_of_employees > 20 and country != 'Ghana, SouthAfrica':
+    print("Congratulations! You are qualified for international expansion")
+else:
+    print("we are sorry to inform you that you do not meet our specific requirements.")'''    
+
+#------------------------------------------------------------------------
+
+#num7
+'''annual_revenue = int(input("What is your Annual Revenue?: "))
+if annual_revenue < 10000:
+    print("Your tax rate is 5%")
+elif annual_revenue < 50000:
+    print("Your tax rate is 10%")    
+elif annual_revenue < 200000:
+    print("Your tax rate is 18%")
+else:      
+    print("Your tax rate is 25%")'''
+
+#----------------------------------------------------------------
+
+
+#num8
+'''registered = str(input("Are you Registered(yes/no)?: "))
+tax_id = str(input("Do you have an existing Tax ID(yes/no)?: "))
+annual_revenue = int(input("What is your Annual Revenue?: "))
+employees = int(input("How many employees do you have?: "))
+operation_years = int(input("How many years have you been in operation?: "))'''
+
+#------------------------------------------------------------------
+#num9
 
 
     
-
-
-
-
-
-
-
-
