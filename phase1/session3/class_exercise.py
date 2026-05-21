c98273bf126642175fa1903a02600607873b8c44
albadarawi <albadarawi@gmail.com>
2026-05-21 10:40:40 +0100

    class exercise

---
+++ phase1/session3/class_exercise.py
@@ -0,0 +1,59 @@
+
+
+#Business_name = str(input("What is your business name: "))
+# owner_name = str(input("What is your name: "))
+#owner_age = int(input("How old are you?: "))
+# business_type = str(input("What business are you into: "))
+# country = str(input("what country are you from: "))
+#monthly_revenue = int(input("How much is your annual revenue?: "))
+#number_of_employees = int(input("How many employees do you have?: "))
+years_in_operation = int(input("How many years do you have in operations?: "))
+#is_registered = str(input("Is your company registerd?"))
+#has_tax_id = str(input("does your company has tax id: "))
+
+"""if owner_age >= 18:
+    print("You are legally allowed to open a company")
+else:
+    print("You are legally not allowed to open a company")
+    
+
+if monthly_revenue < 50000 and years_in_operation >=2:
+    if is_registered == "yes" and has_tax_id == "yes":
+        print("Eligible for Govt. grant")
+    else:
+        print("Not Eligible for govt. grant")
+        
+
+if monthly_revenue > 10000000 or number_of_employees <2 or is_registered == "no":
+    print("This business is flagged suspecious")
+else:
+    print("The business is good to go")
+    
+    
+if monthly_revenue <=20000:
+    print("This businee is a start up business")
+elif monthly_revenue <=39000:
+    print("This businee is Small business")
+elif monthly_revenue <=40000:
+    print("This businee is medium enterprise")
+else:
+    print("thi is a large corporation")
+"""
+
+
+annua_revenue >= 100000:
+if annual_revenue >=100000 or years_in_operation >=10:
+    print("The business is qualifies for premium suppots")
+else:
+    print("Suspicious business is not qualified")
+    
+
+        
+    
+    
+    
+    
+    
+    
+
+
