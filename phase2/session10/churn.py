245cc5c91381a91ef9584cf3487193d3b20a5763
franckoline <franckoline@hotmail.com>
2026-06-08 11:02:48 +0100

    update customer churn

--- phase2/session10/churn.py
+++ phase2/session10/churn.py
@@ -1,6 +1,6 @@
 """
 Session: Ten
-Date: 2nd June 2026 8:30am
+Date: 8th June 2026 9:30am
 focus: Churn Probability
 Facilitator: Olatunbosun
 """
@@ -22,6 +22,9 @@ total_customers = len(customer_data)
 
 
 churn_events = [customer["churned"] for customer in customer_data]
+print("printing customer\n")
+print(churn_events)
+print("printing customer\n")
 total_churned = sum(churn_events)
 
 p_churn = total_churned / total_customers
@@ -34,8 +37,14 @@ print(f"Baseline Probability of Churn: {p_churn:.2f} ({p_churn * 100:.1f}%)")
 
 print("\n--- Predictive Risk Analysis ---")
 
+"""
+Conditional Population
+"""
 # Filter space for high-ticket customers
 high_ticket_customers = [c for c in customer_data if c["support_tickets"] > 2]
+print("printing customer")
+print(high_ticket_customers)
+print("printing customer\n")
 total_high_ticket = len(high_ticket_customers)
 
 # Count how many of these high-ticket customers actually churned
@@ -49,3 +58,9 @@ if total_high_ticket > 0:
     print(f"Probability of Churn if Tickets > 2: {p_churn_given_high_tickets:.2f} ({p_churn_given_high_tickets * 100:.1f}%)")
 else:
     print("No customers matched the criteria.")
+
+
+print("\n\n New Dataset \n\n")
+print(customer_data)
+print('\n\n New data')
+print(high_ticket_customers)
